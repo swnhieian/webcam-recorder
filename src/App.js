99ce828cf92cbdceaaf9aacee4a4f0cc0b9278d6
hker <hker@uw.edu>
2019-06-13 20:14:57 +0800

    cleaned up unnecessary code for fresh start

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
 import React from 'react';
-import Recorder from './Recorder';
-import UserInterface from './UserInterface';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 // -------------------------- Firebase ------------------------- //
 
  
-function Index() {
-  return <h2>Home</h2>;
-}
+
 
 class App extends React.Component {
   render() {
     return (
-      <div className='App'>
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/webcams">WebCams</Link></li>
-                <li><Link to="/user">UserInterface</Link></li>
-              </ul>
-            </nav>
-          </div>
-          <Route path="/" exact component={Index} />
-          <Route path="/webcams" component={Recorder} />
-          <Route path="/user" component={UserInterface} />
-        </Router>
+      <div className='container'>
+        <h1>Hello world!</h1>
+      
       </div>
     );
   };

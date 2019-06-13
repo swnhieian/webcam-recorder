da830134cb2fb8414bb6f0ed0af776ea65bc4878
hker <hker@uw.edu>
2019-06-13 19:28:35 +0800

    will work on sync after

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as firebase from 'firebase/app'
-import firestore from 'firebase/firestore'
+// import * as firebase from 'firebase/app'
+// import firestore from 'firebase/firestore'
 
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyD6YlDRSzOgeB5ppmaQq3eMwW-C3D-aZVY',
-  authDomain: 'webcam-recorder-db.firebaseapp.com',
-  databaseURL: 'https://webcam-recorder-db.firebaseio.com',
-  projectId: 'webcam-recorder-db',
-  storageBucket: 'webcam-recorder-db.appspot.com',
-  messagingSenderId: '261814907199',
-  appId: '1:261814907199:web:19b731560c831f55'
-};
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyD6YlDRSzOgeB5ppmaQq3eMwW-C3D-aZVY',
+//   authDomain: 'webcam-recorder-db.firebaseapp.com',
+//   databaseURL: 'https://webcam-recorder-db.firebaseio.com',
+//   projectId: 'webcam-recorder-db',
+//   storageBucket: 'webcam-recorder-db.appspot.com',
+//   messagingSenderId: '261814907199',
+//   appId: '1:261814907199:web:19b731560c831f55'
+// };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
 // --------------------------- React --------------------------- //
 

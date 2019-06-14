3f2c803e8100dae14e197741f2c6753cc5066203
hker <hker@uw.edu>
2019-06-14 16:29:54 +0800

    added more styling for user input fields

--- src/app/App.js
+++ src/app/App.js
@@ -1,29 +1,58 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+// scss
 import './App.scss'
+
+// svgs
 import tsinghua_logo from '../assets/svg/tsinghua.svg'
 import collapse from '../assets/svg/collapse-chevron.svg'
 // import expand from '../assets/svg/expand-chevron.svg'
 import btn_tester from '../assets/svg/tester-button.svg'
 import sample_cam from '../assets/svg/sample-cam.svg'
 
+// components
+import InputField from '../Components/InputField'
+
 class App extends React.Component {
   render() {
     return (
       <div className='container'>
         <div className='left_panel'>
-          
-          <img id="tsinghua_logo" src={tsinghua_logo} alt='' />
-          <img src={sample_cam} alt=""/>
+          <div className='logo'>
+            <img id='tsinghua_logo' src={tsinghua_logo} alt='' />
+          </div>
+          <div className='cameras'>
+            <img className='cam' src={sample_cam} alt='' />
+            <img className='cam' src={sample_cam} alt='' />
+            <img className='cam' src={sample_cam} alt='' />
+            <img className='cam' src={sample_cam} alt='' />
+            <img className='cam' src={sample_cam} alt='' />
+            <img className='cam' src={sample_cam} alt='' />
+            <img className='cam' src={sample_cam} alt='' />
+            <img className='cam' src={sample_cam} alt='' />
+            <img className='cam' src={sample_cam} alt='' />
+            <img className='cam' src={sample_cam} alt='' />
+            <img className='cam' src={sample_cam} alt='' />
+            <img className='cam' src={sample_cam} alt='' />
+            <img className='cam' src={sample_cam} alt='' />
+            <img className='cam' src={sample_cam} alt='' />
+            <img className='cam' src={sample_cam} alt='' />
+            <img className='cam' src={sample_cam} alt='' />
+            <img className='cam' src={sample_cam} alt='' />
+          </div>
         </div>
-        <div className="right_panel">
-          <div className="nav_bar">
-            <div className="item">
-              <img src={collapse} alt=""/>
+        <div className='right_panel'>
+          <div className='nav_bar'>
+            <div className='item'>
+              <img src={collapse} alt='' />
             </div>
-            <div className="item">
-              <img id="btn_tester" src={btn_tester} alt=""/>
+            <div className='item'>
+              <img id='btn_tester' src={btn_tester} alt='' />
             </div>
+          </div>
+          <div className='data_panel'>
+            <InputField /> 
           </div>
         </div>
       </div>

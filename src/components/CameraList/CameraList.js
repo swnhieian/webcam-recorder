9474c1020e0591c8ba18d2a46a708974696c1d53
hker <hker@uw.edu>
2019-06-15 03:42:29 +0800

    webcams added

--- src/components/CameraList/CameraList.js
+++ src/components/CameraList/CameraList.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react'
-import sample_cam from '../../assets/svg/sample-cam.svg';
+// import sample_cam from '../../assets/svg/sample-cam.svg';
+import Webcam from '../Webcam.js'
 
 export default class CameraList extends Component {
   render() {
+    let num_cams = [...Array(10).keys()];
+    let cams_list = num_cams.map((i) => {
+      return <Webcam key={i} name={"Camera " + (i + 1)}/>
+    });
     return (
       <div>
         <div className='cameras'>
-          <img className='cam' src={sample_cam} alt='' />
-          <img className='cam' src={sample_cam} alt='' />
-          <img className='cam' src={sample_cam} alt='' />
-          <img className='cam' src={sample_cam} alt='' />
-          <img className='cam' src={sample_cam} alt='' />
-          <img className='cam' src={sample_cam} alt='' />
-          <img className='cam' src={sample_cam} alt='' />
-          <img className='cam' src={sample_cam} alt='' />
-          <img className='cam' src={sample_cam} alt='' />
-          <img className='cam' src={sample_cam} alt='' />
-          <img className='cam' src={sample_cam} alt='' />
-          <img className='cam' src={sample_cam} alt='' />
-          <img className='cam' src={sample_cam} alt='' />
-          <img className='cam' src={sample_cam} alt='' />
-          <img className='cam' src={sample_cam} alt='' />
-          <img className='cam' src={sample_cam} alt='' />
-          <img className='cam' src={sample_cam} alt='' />
+          {cams_list}
         </div>
       </div>
     );

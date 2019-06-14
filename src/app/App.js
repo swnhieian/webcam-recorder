5f36c2ddbeb028493b256d95523ddbdd80a72180
hker <hker@uw.edu>
2019-06-14 14:39:06 +0800

    refactor: removed some spacing

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-// -------------------------- Firebase ------------------------- //
-
- 
-
-
 class App extends React.Component {
   render() {
     return (

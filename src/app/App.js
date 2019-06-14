20b1eef851812a3ce42583ddf7b232fffda39af6
hker <hker@uw.edu>
2019-06-14 15:38:57 +0800

    added some basic styling for admin portal

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './App.scss'
+import tsinghua_logo from '../assets/svg/tsinghua.svg'
+import collapse from '../assets/svg/collapse-chevron.svg'
+// import expand from '../assets/svg/expand-chevron.svg'
+import btn_tester from '../assets/svg/tester-button.svg'
+import sample_cam from '../assets/svg/sample-cam.svg'
 
 class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        <h1>Hello world!</h1>
-      
+        <div className='left_panel'>
+          
+          <img id="tsinghua_logo" src={tsinghua_logo} alt='' />
+          <img src={sample_cam} alt=""/>
+        </div>
+        <div className="right_panel">
+          <div className="nav_bar">
+            <div className="item">
+              <img src={collapse} alt=""/>
+            </div>
+            <div className="item">
+              <img id="btn_tester" src={btn_tester} alt=""/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

07c13f85664d46e10ba8b4cea9441e0ab92f2d28
hker <hker@uw.edu>
2019-06-14 21:52:40 +0800

    wip: table pagination

--- src/components/Table/TableFooter.js
+++ src/components/Table/TableFooter.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import './TableFooter.scss'
 import PageNum from './PageNum.js'
+import next from '../../assets/svg/collapse-chevron.svg'
+import prev from '../../assets/svg/expand-chevron.svg';
 
 export default class TableFooter extends Component {
   render() {
     let nums = []
-    for (let i = 1; i < 15; i++) {
+    for (let i = 1; i < 9; i++) {
       nums.push(<PageNum num={i}/>);
     }
     return (
@@ -18,7 +20,13 @@ export default class TableFooter extends Component {
                 <input type='text' />
               </form>
             </div>
-            <div className='right_footer'>{nums}</div>
+            <div className='right_footer'>
+              <img src={prev} alt='' className='small_btn' />
+              {nums}
+              <p>...</p>
+              <PageNum num={20} />
+              <img src={next} alt='' className='small_btn' />
+            </div>
           </div>
         </div>
       </div>

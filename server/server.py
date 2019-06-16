ee1364f78df4d06633111e1b3a138f0104ec9bbe
Weinan Shi <swnhieian@126.com>
2019-06-16 22:00:21 +0800

    added Weinan's play sync audio code to server.py

--- server/server.py
+++ server/server.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, send_from_directory, url_for
+from threading import Thread
+from http.server import SimpleHTTPRequestHandler
+from socketserver import TCPServer
+from soco.discovery import by_name, discover
+import os
 app = Flask(__name__)
+from soco import SoCo
 
 start_state = False
 
@@ -15,3 +21,15 @@ def getStartState():
 def startingTest():
   start_state = True
   return "starting test"
+
+@app.route('/<filename>')
+def files(filename):
+    directory = os.getcwd()
+    return app.send_static_file(filename)
+    
+@app.route('/play')
+def play():
+    sonos = SoCo('192.168.0.103')
+    uri = 'http://192.168.0.100:5000/out_sine.wav'
+    sonos.play_uri(uri)
+    return 'play'

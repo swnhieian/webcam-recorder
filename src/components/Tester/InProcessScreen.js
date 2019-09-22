fae85fd4471c8a3466f3eab984e7e95e6e3a01c0
Sean Ker <s3and0s@gmail.com>
2019-09-23 00:28:46 +0800

    gh pages

--- src/components/Tester/InProcessScreen.js
+++ src/components/Tester/InProcessScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import NameField from '../NameField/NameField';
 import qs from '../../utils/qs';

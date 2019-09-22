94f4e19f85b8d3de725c94a108b19e8b897a859e
Sean Ker <s3and0s@gmail.com>
2019-09-23 00:28:08 +0800

    Updates

---
+++ precache-manifest.dcfe678ee252161f15f4bc9ea3098e3f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27eab0acdfe733313a92d924f1de5bb0",
+    "url": "/webcam-recorder/index.html"
+  },
+  {
+    "revision": "bb374d08fbff7f951acf",
+    "url": "/webcam-recorder/static/css/2.57125576.chunk.css"
+  },
+  {
+    "revision": "6ad3b1505f58dfe13b53",
+    "url": "/webcam-recorder/static/css/main.39db5d54.chunk.css"
+  },
+  {
+    "revision": "bb374d08fbff7f951acf",
+    "url": "/webcam-recorder/static/js/2.e2dcaf49.chunk.js"
+  },
+  {
+    "revision": "6ad3b1505f58dfe13b53",
+    "url": "/webcam-recorder/static/js/main.666e0fdb.chunk.js"
+  },
+  {
+    "revision": "f63a25f1104aadb09a1c",
+    "url": "/webcam-recorder/static/js/runtime~main.34a2a1d2.js"
+  },
+  {
+    "revision": "565428a5b6f126c7dad0ca6c2e31dd3e",
+    "url": "/webcam-recorder/static/media/beg.565428a5.svg"
+  },
+  {
+    "revision": "126ea5d6d352791839abd91a6fe72a64",
+    "url": "/webcam-recorder/static/media/collapse-chevron.126ea5d6.svg"
+  },
+  {
+    "revision": "b927e1019c81f3e25f0bf012a3fbefe4",
+    "url": "/webcam-recorder/static/media/end.b927e101.svg"
+  },
+  {
+    "revision": "fd7409a77b543b06706cc6e3566e35d8",
+    "url": "/webcam-recorder/static/media/expand-chevron.fd7409a7.svg"
+  },
+  {
+    "revision": "d9eb1bf673561e5d91ece955ab55b9ab",
+    "url": "/webcam-recorder/static/media/sentences-english-test.d9eb1bf6.txt"
+  }
+]);

0361ae389822a3c74a9ef99f724e3c6d084075c9
Sean Ker <s3and0s@gmail.com>
2019-10-01 11:05:45 +0800

    reset server id

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './app/App.js';
 import io from 'socket.io-client'; 
 
-const socket = io('http://192.168.0.100:5000');
+const socket = io('http://192.168.0.102:5000');
 
 ReactDOM.render(<App socket={socket} />, document.getElementById('root'));

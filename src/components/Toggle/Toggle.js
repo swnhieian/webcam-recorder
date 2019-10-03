daede23afab905426c660e09f5bafc7eff4e57f0
Sean Ker <s3and0s@gmail.com>
2019-10-04 07:25:16 +0800

    componentized toggle

--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -1,17 +1,35 @@
-import React from 'react'
+/**
+ * **Based on: http://www.cssscript.com/demo/pure-css-css3-smooth-toggle-switch/**
+ */
+
+import React, { useEffect }from 'react'
 import './toggle.scss'
 import PropTypes from 'prop-types';
 
+/**
+ * 
+ * @param {*} props 
+ */
 export default function Toggle(props) {
+  useEffect(() => {
+    const toggle = document.getElementById(props.id);
+    try {
+      toggle.addEventListener('change', () => {
+        props.onChangeFunc(toggle.checked);
+      });
+    } catch (NotYetLoadedException) {
+      //
+    }
+  }, [])
   return (
     <div className="switch">
       <input type="checkbox" name="toggle" id={props.id}/>
       <label htmlFor="toggle"><i></i></label>
-      <span></span> 
-    </div>
+    </div>  
   )
 }
 
 Toggle.propTypes = {
-  id: PropTypes.string.isRequired
+  id: PropTypes.string.isRequired,
+  onChangeFunc: PropTypes.func.isRequired
 }

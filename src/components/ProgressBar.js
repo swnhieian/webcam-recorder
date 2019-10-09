9208524813ee7e77ddc8e1725b959936a2ce51a6
Sean Ker <s3and0s@gmail.com>
2019-10-09 16:53:16 +0800

    added circle to progress bar component

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,22 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import { Line } from 'rc-progress';
+import { Line, Circle } from 'rc-progress';
 
 export default function ProgressBar(props) {
   const percent = ((props.curr ) / (props.total + 1) * 100).toFixed(2);
   const alignmentStyle = props.align === 'left' ? { margin: '0' } : {};
   return (
-    <div className='progress_bar' style={alignmentStyle}>
-      <pre>
-        进度: {props.curr} / {props.total + 1} ({percent}%)
+    <div className="progress_bar" style={alignmentStyle}>
+      <pre style={{color: props.strokeColor}}>
+        {props.title}: {props.curr} / {props.total + 1} ({percent}%)
       </pre>
+      {props.shape === 'line' &&
       <Line
+        className="line-progress"
         percent={percent}
         strokeWidth={props.strokeWidth}
         trailWidth={props.strokeWidth}
-        strokeColor = '#3bb8ce'
+        strokeColor = {props.strokeColor}
         trailColor='#363732'
-      />
+      />}
+      {props.shape === 'circle' &&
+      <Circle 
+        className="circle-progress"
+        percent={percent}
+        strokeWidth={props.strokeWidth}
+        trailWidth={props.strokeWidth}
+        strokeColor = {props.strokeColor}
+        trailColor='#363732'
+      />}
     </div>
   );
 }
@@ -26,4 +37,7 @@ ProgressBar.propTypes = {
   total: PropTypes.number.isRequired,
   align: PropTypes.string.isRequired,
   strokeWidth: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  shape: PropTypes.string.isRequired,
+  strokeColor: PropTypes.string.isRequired,
 };
